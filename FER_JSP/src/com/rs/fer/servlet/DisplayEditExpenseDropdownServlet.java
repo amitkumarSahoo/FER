0d44bbc674b94d54a3bbaafaf9cf5f6b0f4efb2f
amitk <amitk@DESKTOP-VGFQS74>
2018-02-22 20:34:44 +0530

    test2

--- FER_JSP/src/com/rs/fer/servlet/DisplayEditExpenseDropdownServlet.java
+++ FER_JSP/src/com/rs/fer/servlet/DisplayEditExpenseDropdownServlet.java
@@ -30,8 +30,6 @@ public class DisplayEditExpenseDropdownServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		
-		//changes
 
 		HTMLUtil.generateHeaderAndLeftFrame(out);
 		List<Expense> expenses = ferService.getExpenses();
